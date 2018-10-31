cbfa80d27acbb8a592efb2160f596421192f81ff
Shangting Li <shangtingli@rinaoniwas-MacBook-Pro.local>
2018-10-30 20:20:58 -0700

    Version 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import {TopBar} from './TopBar.js';
-
+import {Register} from "./Register";
+import {Main} from './Main';
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <TopBar/>
+                <Main/>
+                {/*<Register/>*/}
             </div>
         );
     }
